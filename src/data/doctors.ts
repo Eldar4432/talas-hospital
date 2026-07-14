ecbc0ba19f598bbd3c51f3cae84252b7c4461ed9
Eldar4432 <aibekov4432@gmail.com>
2026-07-14 12:22:33 +0600

    Created folder data and added doctors.ts

---
+++ src/data/doctors.ts
@@ -0,0 +1,20 @@
+export const doctors = [
+  {
+    id: 1,
+    name: "Иван Иванов",
+    position: "Хирург",
+    experience: "15 лет",
+  },
+  {
+    id: 2,
+    name: "Айгуль Садыкова",
+    position: "Кардиолог",
+    experience: "12 лет",
+  },
+  {
+    id: 3,
+    name: "Бакыт Токтосунов",
+    position: "Терапевт",
+    experience: "10 лет",
+  },
+];
