4451fc6e4cc799e3715186921a3851c2efd7b705
Eldar4432 <aibekov4432@gmail.com>
2026-07-15 12:14:47 +0600

    Add doctors API endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "./database/db";
+import doctorsRouter from "./routes/doctors";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/doctors", doctorsRouter);
 
 app.get("/", (req, res) => {
   res.json({
